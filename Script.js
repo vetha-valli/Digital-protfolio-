7e14840774a55dc44247c5907bf1af20f061d2fb
vetha-valli <vetha390@gmail.com>
2025-08-28 19:42:47 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,5 @@
+// Contact form action
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Your message has been sent successfully!");
+});
